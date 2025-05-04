fd339df06a103d559dd18b600ca69ab1b604f79f
faris771 <farisabufarha33@gmail.com>
2025-05-04 13:47:24 +0300

    Reads & shows video ✅

--- main.py
+++ main.py
@@ -6,11 +6,24 @@ import numpy as np
 
 
 
+CARS_VIDEO_PATH = 'videos/cars.mp4'
+WIDTH_ID, HEIGHT_ID,  = 3,4
+
+
 def main():
 
-    pass
+    cap = cv2.VideoCapture(CARS_VIDEO_PATH)
+    cap.set(WIDTH_ID, 720)
+    cap.set(HEIGHT_ID, 480)
+
+    while True:
+
+        success, frame = cap.read()
+        cv2.imshow("Live Footage", frame)
 
 
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+            break
 
 
 if __name__ == '__main__':
