7ddee29aa67c380a16afc42828aaf51c6d7d8772
faris771 <farisabufarha33@gmail.com>
2025-05-04 21:27:03 +0300

    All features implemented✅

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ def main():
     # min_hits=3: This defines the minimum number of consecutive frames an object must be detected before it is considered a valid track.
     # iou_threshold=0.3: This is the Intersection over Union (IoU) threshold used to associate detections with existing tracks. If the IoU between a detection and a track is below this value, they are not associated.
 
+    car_counter = 0
+    # cars_set = set()
+    crossed_ids = {}
+
+    current_max_id = -1
+
     while True:
 
         success, frame = cap.read()
@@ -30,8 +36,7 @@ def main():
         # each row is a detected object
 
         cv2.line(frame, (const.LIMIT_LINE['x1'], const.LIMIT_LINE['y1']),
-                 (const.LIMIT_LINE['x2'], const.LIMIT_LINE['y2']),(0,0,255),thickness=5)
-
+                 (const.LIMIT_LINE['x2'], const.LIMIT_LINE['y2']), (0, 0, 255), thickness=5)
 
         for result in yolo_results:
             # typically it's just one  result per frame but because it's a Python generator we have to loop through
@@ -63,14 +68,29 @@ def main():
         for result in tracker_results:
             x1, y1, x2, y2, id = result
             x1, y1, x2, y2, id = int(x1), int(y1), int(x2), int(y2), int(id)
+
             box_width = x2 - x1
             box_height = y2 - y1
+
             cvzone.cornerRect(frame, (x1, y1, box_width, box_height,), colorR=(255, 0, 0))
             cvzone.putTextRect(frame, f"{id}", (max(const.MIN_TEXT_X, x1), max(const.MIN_TEXT_Y, y1)), 1, 1)
+            cvzone.putTextRect(frame, f"Car Count: {car_counter}", (20, 30), 2, 1)
+
+            # draw the circle
+            cx, cy = x1 + box_width // 2, y1 + box_height // 2,
+            cv2.circle(frame, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+            # check if center passed the line
+            if const.LIMIT_LINE['x1'] <= cx and cx <= const.LIMIT_LINE['x2'] \
+                    and const.LIMIT_LINE['y1'] - 20 <= cy and cy <= const.LIMIT_LINE['y2'] + 20:
+
+                # check if car already crossed and registered in the Dict
+                if not id in crossed_ids:
+                    car_counter += 1
+                    crossed_ids[id] = True
+
 
         cv2.imshow(const.FRAME_TITLE, frame)
-        # cv2.imshow('shaded',shaded_frame)
-
         if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
             break
 
