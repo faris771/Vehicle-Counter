93b797f355ee6c41fa5a783c8c10427b83942a29
faris771 <farisabufarha33@gmail.com>
2025-05-04 14:02:10 +0300

    Objects detected with bounding boxes

---
+++ constants.py
@@ -0,0 +1,8 @@
+CARS_VIDEO_PATH = 'videos/cars.mp4'
+WIDTH_ID, HEIGHT_ID,  = 3,4
+YOLO_PATH = 'yolo_weights/yolov8n.pt'
+VIDEO_WIDTH_PIXELS = 720
+VIDEO_HEIGHT_PIXELS = 420
+FRAME_TITLE= 'Live Footage'
+
+
--- main.py
+++ main.py
@@ -2,31 +2,41 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 import numpy as np
-
-
-
-
-CARS_VIDEO_PATH = 'videos/cars.mp4'
-WIDTH_ID, HEIGHT_ID,  = 3,4
+import constants as const
 
 
 def main():
+    cap = cv2.VideoCapture(const.CARS_VIDEO_PATH)
+    cap.set(const.WIDTH_ID, const.VIDEO_WIDTH_PIXELS)
+    cap.set(const.HEIGHT_ID, const.VIDEO_HEIGHT_PIXELS)
 
-    cap = cv2.VideoCapture(CARS_VIDEO_PATH)
-    cap.set(WIDTH_ID, 720)
-    cap.set(HEIGHT_ID, 480)
+    yolo_model = YOLO(const.YOLO_PATH)
 
     while True:
 
         success, frame = cap.read()
-        cv2.imshow("Live Footage", frame)
+        yolo_results = yolo_model(frame, stream=True)  # frame yolo results
 
+        for result in yolo_results:
+            boxes = result.boxes
+
+        for box in boxes:
+
+            # get  each coordinates of every bounding box
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
+
+            box_width = x2-x1
+            box_height = y2-y1
+            cvzone.cornerRect(frame,(x1,y1,box_width,box_height))
+
+
+
+        cv2.imshow(const.FRAME_TITLE, frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
             break
 
 
 if __name__ == '__main__':
-
     main()
-
